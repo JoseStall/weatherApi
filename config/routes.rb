3aa01e6d1cedb75744ab3aff520adfaa94cf4c3c
guillaume <g.delory62@gmail.com>
2018-06-19 17:05:42 +0200

    ajout api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'home/show'
+  root 'home#index'
+  get 'home/show', to: 'home#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
