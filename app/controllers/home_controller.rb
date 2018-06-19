36d1e1f3336c09e131862b4f4469638505e4f0e7
guillaume <g.delory62@gmail.com>
2018-06-19 18:00:22 +0200

    widget openweather

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
+require 'dotenv'
+require 'open_weather'
+Dotenv.load('script.env')
+
 class HomeController < ApplicationController
   def index
+  	
   end
 
   def show
+  	options = { units: "metric", APPID: ENV['weatherkey'] , lang: 'fr'}
+	puts OpenWeather::Current.city(params[:address], options)
   end
 end
