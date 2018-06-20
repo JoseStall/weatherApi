b0e04d4e998679bc7d8ebd0f4e30f5735651d5a8
guillaume <g.delory62@gmail.com>
2018-06-20 12:29:36 +0200

    test final

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,18 @@ Dotenv.load('script.env')
 
 class HomeController < ApplicationController
 	def index
+# configuration de la gem openweather2
 		Openweather2.configure do |config|
 			config.endpoint = 'http://api.openweathermap.org/data/2.5/weather'
 			config.apikey = ENV['weatherkey']
 		end
-		if params[:address] !=nil
-			@meteo =  Openweather2.get_weather(city: params[:address], units: 'metric', lang: 'fr')
+# 		
+		if params[:address] != nil 
+			begin
+				@meteo =  Openweather2.get_weather(city: params[:address], units: 'metric', lang: 'fr')
+			rescue Openweather2::UnknownResponse
+				@meteo = 0
+			end
 		end
-	end
-
-	def show
-
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'home/show', to: 'home#show', as: 'show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
