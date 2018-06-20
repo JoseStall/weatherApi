f0857a7d816712d10e58cf05d15a59e46761c24e
guillaume <g.delory62@gmail.com>
2018-06-20 10:21:59 +0200

    heroku app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,10 @@ Dotenv.load('script.env')
 
 class HomeController < ApplicationController
 	def index
+		city = params[:address]
+		puts city.to_s
 		@options = { units: "metric", APPID: ENV['weatherkey'] , lang: 'fr'}
+		@meteo =  OpenWeather::Current.city(city.to_s, @options)
 		
 	end
 
